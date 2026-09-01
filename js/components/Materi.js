d4ca655d5c2099bb437f9af62589ca3dc870ea86
Afta Putra <contactinopu@gmail.com>
2026-09-01 12:26:53 +0700

    Update Materi.js

--- js/components/Materi.js
+++ js/components/Materi.js
@@ -3,7 +3,7 @@ import Auth from '../auth.js';
 import { supabase } from '../config/supabase.js';
 
 class Materi {
-    static currentWeek = 4;
+    static currentWeek = 5;
     static currentCourseIndex = 0;
     static currentVideoIndex = 0;
     static auth = new Auth();
@@ -22,7 +22,7 @@ class Materi {
     ];
     
     // Variable untuk menentukan week mana yang terbuka untuk regular user
-    static unlockedWeekForRegular = 4;
+    static unlockedWeekForRegular = 5;
     
     // Flag untuk menandai apakah user whitelist
     static isWhitelisted = false;

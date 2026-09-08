478afdd28deb02c3b4db38537dba1a3d2e3be468
Afta Putra <contactinopu@gmail.com>
2026-09-08 14:31:24 +0700

    Update Dashboard.js

--- js/components/Dashboard.js
+++ js/components/Dashboard.js
@@ -3,7 +3,7 @@ import { getWeekData } from '../../data/weekData.js';
 import { courseAttendanceService } from '../config/supabase.js';
 
 class Dashboard {
-    static unlockedWeek = 5; // Hanya week 1 yang terbuka
+    static unlockedWeek = 6; // Hanya week 1 yang terbuka
 
     static async render() {
         const progress = await courseAttendanceService.getUserCourseProgress();
